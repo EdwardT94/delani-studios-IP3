e5ade30d4365445456dda4895a2dc9ba1714a004
EdwardT94 <edwardmutisyathomas@gmail.com>
2019-08-26 08:24:58 +0300

    I added js code on portfolio on toggling content

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $("#des").click(function() {
+      $("#des").toggle();
+      $("#content1").toggle();
+ });
+});
+$(document).ready(function(){
+    $("#content1").click(function() {
+      $("#des").toggle();
+      $("#content1").toggle();
+ });
+});
+$(document).ready(function(){
+    $("#dev").click(function() {
+      $("#dev").toggle();
+      $("#content2").toggle();
+ });
+});
+$(document).ready(function(){
+    $("#content2").click(function() {
+      $("#dev").toggle();
+      $("#content2").toggle();
+ });
+});
+$(document).ready(function(){
+    $("#pdg").click(function() {
+      $("#pdg").toggle();
+      $("#content3").toggle();
+ });
+});
+$(document).ready(function(){
+    $("#content3").click(function() {
+      $("#pdg").toggle();
+      $("#content3").toggle();
+ });
+});
